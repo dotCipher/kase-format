8ca1917451c7be40cad1ae3e41ae65ae94f78372
Cody Moore <cody@bloomcred.it>
2019-09-09 21:53:33 -0400

    Fixed determine and associated functionality

--- src/commonMain/kotlin/io/dotcipher/kase/KaseFormat.kt
+++ src/commonMain/kotlin/io/dotcipher/kase/KaseFormat.kt
@@ -14,7 +14,7 @@ enum class KaseFormat(
         override val absentTokens: Set<CharToken> = setOf(
             CharToken.UNDERSCORE,
             CharToken.SPACE,
-            CharToken.OTHER_SPECIALS
+            CharToken.OTHER_SPECIAL
         )
         override val matchMaxSequentialUppercase: (Int) -> Boolean = {
             // There should be no uppercase characters
@@ -22,7 +22,7 @@ enum class KaseFormat(
         }
         override val shouldStartWith: CharToken = CharToken.LOWERCASE_LETTER
         override val shouldStartWithIgnore: Set<CharToken> = setOf(
-            CharToken.NUMBERS,
+            CharToken.NUMBER,
             CharToken.HYPHEN
         )
 
@@ -39,7 +39,7 @@ enum class KaseFormat(
         override val absentTokens: Set<CharToken> = setOf(
             CharToken.UNDERSCORE,
             CharToken.SPACE,
-            CharToken.OTHER_SPECIALS
+            CharToken.OTHER_SPECIAL
         )
         override val matchMaxSequentialUppercase: (Int) -> Boolean = {
             // There should be uppercased characters, and they should be isolated
@@ -48,7 +48,7 @@ enum class KaseFormat(
         override val shouldStartWith: CharToken = CharToken.UPPERCASE_LETTER
         // Ignore prefixed numbers or hyphen when checking `shouldStartWith`
         override val shouldStartWithIgnore: Set<CharToken> = setOf(
-            CharToken.NUMBERS,
+            CharToken.NUMBER,
             CharToken.HYPHEN
         )
 
@@ -65,7 +65,7 @@ enum class KaseFormat(
             CharToken.LOWERCASE_LETTER,
             CharToken.UNDERSCORE,
             CharToken.SPACE,
-            CharToken.OTHER_SPECIALS
+            CharToken.OTHER_SPECIAL
         )
         override val matchMaxSequentialUppercase: (Int) -> Boolean = {
             // Match all circumstances, since we restrict on lower case letters already
@@ -73,7 +73,7 @@ enum class KaseFormat(
         }
         override val shouldStartWith: CharToken = CharToken.UPPERCASE_LETTER
         override val shouldStartWithIgnore: Set<CharToken> = setOf(
-            CharToken.NUMBERS,
+            CharToken.NUMBER,
             CharToken.HYPHEN
         )
 
@@ -89,7 +89,7 @@ enum class KaseFormat(
         override val absentTokens: Set<CharToken> = setOf(
             CharToken.HYPHEN,
             CharToken.SPACE,
-            CharToken.OTHER_SPECIALS
+            CharToken.OTHER_SPECIAL
         )
         override val matchMaxSequentialUppercase: (Int) -> Boolean = {
             // There should be no uppercase characters
@@ -97,7 +97,7 @@ enum class KaseFormat(
         }
         override val shouldStartWith: CharToken = CharToken.LOWERCASE_LETTER
         override val shouldStartWithIgnore: Set<CharToken> = setOf(
-            CharToken.NUMBERS,
+            CharToken.NUMBER,
             CharToken.UNDERSCORE
         )
 
@@ -114,7 +114,7 @@ enum class KaseFormat(
         override val absentTokens: Set<CharToken> = setOf(
             CharToken.HYPHEN,
             CharToken.SPACE,
-            CharToken.OTHER_SPECIALS
+            CharToken.OTHER_SPECIAL
         )
         override val matchMaxSequentialUppercase: (Int) -> Boolean = {
             // There should be uppercased characters, and they should be isolated
@@ -122,7 +122,7 @@ enum class KaseFormat(
         }
         override val shouldStartWith: CharToken = CharToken.UPPERCASE_LETTER
         override val shouldStartWithIgnore: Set<CharToken> = setOf(
-            CharToken.NUMBERS,
+            CharToken.NUMBER,
             CharToken.UNDERSCORE
         )
 
@@ -139,7 +139,7 @@ enum class KaseFormat(
             CharToken.LOWERCASE_LETTER,
             CharToken.HYPHEN,
             CharToken.SPACE,
-            CharToken.OTHER_SPECIALS
+            CharToken.OTHER_SPECIAL
         )
         override val matchMaxSequentialUppercase: (Int) -> Boolean = {
             // Match all circumstances, since we restrict on lower case letters already
@@ -147,7 +147,7 @@ enum class KaseFormat(
         }
         override val shouldStartWith: CharToken = CharToken.UPPERCASE_LETTER
         override val shouldStartWithIgnore: Set<CharToken> = setOf(
-            CharToken.NUMBERS,
+            CharToken.NUMBER,
             CharToken.UNDERSCORE
         )
 
@@ -163,7 +163,7 @@ enum class KaseFormat(
             CharToken.UNDERSCORE,
             CharToken.HYPHEN,
             CharToken.SPACE,
-            CharToken.OTHER_SPECIALS
+            CharToken.OTHER_SPECIAL
         )
         override val matchMaxSequentialUppercase: (Int) -> Boolean = {
             // There could be uppercased characters, but they should be isolated
@@ -171,13 +171,19 @@ enum class KaseFormat(
         }
         override val shouldStartWith: CharToken = CharToken.LOWERCASE_LETTER
         override val shouldStartWithIgnore: Set<CharToken> = setOf(
-            CharToken.NUMBERS
+            CharToken.NUMBER
         )
 
         override fun normalizeWord(word: String, wordIndex: Int): String =
             when (wordIndex) {
                 0 -> word.toLowerCase()
-                else -> word.capitalize()
+                else -> when {
+                    word.isEmpty() -> word
+                    word.length == 1 -> word.capitalize()
+                    else -> {
+                        "${word[0].toUpperCase()}${word.slice(1 until word.length).toLowerCase()}"
+                    }
+                }
             }
     },
 
@@ -191,19 +197,63 @@ enum class KaseFormat(
             CharToken.UNDERSCORE,
             CharToken.HYPHEN,
             CharToken.SPACE,
-            CharToken.OTHER_SPECIALS
+            CharToken.OTHER_SPECIAL
         )
         override val matchMaxSequentialUppercase: (Int) -> Boolean = {
-            // There should be uppercased characters, and they should be isolated
-            it == 1
+            // There should be uppercased characters, but they don't need to be isolated
+            it >= 1
         }
         override val shouldStartWith: CharToken = CharToken.UPPERCASE_LETTER
-        override fun normalizeWord(word: String, wordIndex: Int): String = word.capitalize()
+        override fun normalizeWord(word: String, wordIndex: Int): String {
+            return when {
+                word.isEmpty() -> word
+                word.length == 1 -> word.capitalize()
+                else -> {
+                    "${word[0].toUpperCase()}${word.slice(1 until word.length).toLowerCase()}"
+                }
+            }
+        }
     };
+
+    private fun splitWords(input: String, sourceFormat: KaseFormat): List<String> =
+        if (delimiter.isEmpty() && (sourceFormat == CAPITAL_CAMEL || sourceFormat == LOWER_CAMEL)) {
+            // Generate lookahead regex for formats without a delimiter (generally camel case)
+            val firstSplit = input.split(Regex("(?=${boundary.pattern})"))
+            // Then do a rejoin based on the lookahead tokens
+            val rejoinedWords = mutableListOf<String>()
+            var wordCache = StringBuilder()
+            firstSplit.forEachIndexed { wordIndex, string ->
+                if (wordIndex < firstSplit.size - 1) {
+                    // Use lookahead for handling the non-delimiter words
+                    val nextToken = firstSplit.getOrNull(wordIndex + 1)
+                    if (nextToken != null) {
+                        if (!nextToken.matches(boundary)) {
+                            wordCache.append(string)
+                            if (wordCache.isNotEmpty()) {
+                                rejoinedWords.add(wordCache.toString())
+                                wordCache = StringBuilder()
+                            }
+                        } else if (string.length == 1){
+                            wordCache.append(string)
+                        } else if (string.isNotEmpty()) {
+                            rejoinedWords.add(string)
+                        }
+                    }
+                } else {
+                    wordCache.append(string)
+                    if (wordCache.isNotEmpty()) {
+                        rejoinedWords.add(wordCache.toString())
+                    }
+                }
+            }
+            rejoinedWords
+        } else {
+            input.split(boundary)
+        }
 
     fun convert(format: KaseFormat, input: String): String {
         val string = StringBuilder()
-        val split = input.split(boundary)
+        val split = splitWords(input, this)
         split.forEachIndexed { wordIndex, word ->
             if (wordIndex < split.size - 1) {
                 string.append(format.delimiter)
@@ -222,6 +272,8 @@ enum class KaseFormat(
     internal open val shouldStartWith: CharToken? = null
     internal open val shouldStartWithIgnore: Set<CharToken> = emptySet()
 
+    private val dynamicConverter by lazy { DynamicConverter(this) }
+
     abstract fun normalizeWord(word: String, wordIndex: Int = 0): String
 
     private fun matchesRequiredTokens(tokenized: TokenizedString): Boolean =
@@ -238,7 +290,7 @@ enum class KaseFormat(
             tokenized.startsWith(it, shouldStartWithIgnore)
         } ?: true
 
-    fun converter(): DynamicKaseConverter = DynamicKaseConverter(this)
+    fun converter(): DynamicConverter = dynamicConverter
 
     fun converter(sourceFormat: KaseFormat): BiDirectionalConverter =
         BiDirectionalConverter(sourceFormat, this)
@@ -250,9 +302,48 @@ enum class KaseFormat(
                 && matchesStartsWith(tokenized)
 
     companion object {
+
+        private fun String.containsOnly(vararg charRange: CharRange): Boolean {
+            for (char in this) {
+                var matchesAtLeastOneRange = false
+                for (range in charRange) {
+                    if (range.contains(char)) {
+                        matchesAtLeastOneRange = true
+                    }
+                }
+                if (!matchesAtLeastOneRange) {
+                    return false
+                }
+            }
+            return true
+        }
+
+        private fun String.containsOnlyUpperCaseOrNumeric(): Boolean =
+            containsOnly('A'..'Z', '0'..'9')
+
+        private fun String.containsOnlyLowerCaseOrNumeric(): Boolean =
+            containsOnly('a'..'z', '0'..'9')
+
         fun determine(string: String): KaseFormat? {
+            // Short circuit
+            if (string.isBlank()) {
+                return null
+            }
+            // Then parse
             val tokenized = TokenizedString.of(string)
-            return values().firstOrNull { it.matches(tokenized) }
+            val maybeFormat = values().firstOrNull { it.matches(tokenized) }
+            if (maybeFormat != null) {
+                return maybeFormat
+            }
+            // Best guesses if only all lower or upper
+            if (string.containsOnlyUpperCaseOrNumeric()) {
+                return UPPER_UNDERSCORE
+            }
+            if (string.containsOnlyLowerCaseOrNumeric()) {
+                return LOWER_CAMEL
+            }
+            // Give up
+            return null
         }
     }
 
